f0c2860222644f5f1e5935e1a893a170680e6900
_ext Kotsabiuk, Mykola <mykola.kotsabiuk.ext@dieboldnixdorf.com>
2020-01-23 19:22:08 +0200

    Updates

---
+++ precache-manifest.9e3ffdf5d7687ce91785c04129e37481.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea085758101d31d227211dae691cbbc9",
+    "url": "/climhub/index.html"
+  },
+  {
+    "revision": "fad8ad06311f71aec7aa",
+    "url": "/climhub/static/css/2.540d54db.chunk.css"
+  },
+  {
+    "revision": "5811a97ed24d9ef9f623",
+    "url": "/climhub/static/css/main.a2e27934.chunk.css"
+  },
+  {
+    "revision": "fad8ad06311f71aec7aa",
+    "url": "/climhub/static/js/2.85968467.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/climhub/static/js/2.85968467.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5811a97ed24d9ef9f623",
+    "url": "/climhub/static/js/main.8f388d56.chunk.js"
+  },
+  {
+    "revision": "56bfd4c5264a0080ebd3",
+    "url": "/climhub/static/js/runtime-main.9c96ac9f.js"
+  },
+  {
+    "revision": "73f5ddc99031f6aa9f4c7299d55107fe",
+    "url": "/climhub/static/media/wallpaper.73f5ddc9.png"
+  },
+  {
+    "revision": "2ca3df0981116e195ebc62dd1dcc8b9b",
+    "url": "/climhub/static/media/weathericons-regular-webfont.2ca3df09.ttf"
+  },
+  {
+    "revision": "b2233bc1525939550405c87cffede242",
+    "url": "/climhub/static/media/weathericons-regular-webfont.b2233bc1.svg"
+  },
+  {
+    "revision": "e7ef2b448d27cf5312a73ceb3e7841c4",
+    "url": "/climhub/static/media/weathericons-regular-webfont.e7ef2b44.eot"
+  },
+  {
+    "revision": "f5b0d0d9cb02e551003132e079435312",
+    "url": "/climhub/static/media/weathericons-regular-webfont.f5b0d0d9.woff"
+  }
+]);
